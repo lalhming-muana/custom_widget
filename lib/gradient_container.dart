9281a2e0b934f3852d390f8de16df924f074974f
lhmuana <edd_vai@yahoo.co.in>
2025-11-23 19:03:32 +0530

    two custom widget created

---
+++ lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:custom_widget/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color.fromARGB(255, 145, 97, 228), Colors.deepPurple],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:custom_widget/gradient_container.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(),
-      ),
-    ),
-  );
-}
-
-class GradientContainer extends StatelessWidget{
-
- @override
-  Widget build(BuildContext context) {
-    
-    return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 145, 97, 228),
-                Colors.deepPurple,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(child: Text('Custom Widget')),
-        );
-  }
-
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
